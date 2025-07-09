020a12c8715c4776492c8b011ce5d958914fb4e8
heeezni <hjin3456@naver.com>
2025-07-09 12:59:44 +0900

    0709 오전수업 마무리

--- src/main/java/com/sinse/mvcappp/controller/DispatcherServlet.java
+++ src/main/java/com/sinse/mvcappp/controller/DispatcherServlet.java
@@ -27,6 +27,13 @@ public class DispatcherServlet extends HttpServlet{
 		doRequest(request, response);
 	}
 	
+	/**
+	 * 이 코드의 단점 : 서블릿 매핑이 if문으로 바뀐 것 뿐 (매 요청마다 if문 만들어야함)
+	 * 매 요청마다 1:1 대응되는 매핑을 피하기 위해 하나의 진입점으로 몰았으나
+	 * 진입점이 되는 클래스가 매 요청마다 1:1 대응되는 if 조건문이 작성되고 있음 
+	 *
+	 * 어떻게 해야 if문을 없앨 수 있을꺄? 
+	 * */
 	protected void doRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		if(request.getRequestURI().equals("/blood.do")) { // 클라이언트의 요청이 혈액형이면
@@ -57,6 +64,7 @@ public class DispatcherServlet extends HttpServlet{
 			
 		}
 	}
+	
 
 
 }
